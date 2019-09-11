eacd2ba766dc1fa9306f5d29568845420c03049f
Vasya Pupkin <nomail@ya.ru>
2019-09-11 16:38:02 +0300

    makefile and some stub tests

---
+++ Makefile
@@ -0,0 +1,22 @@
+NAME = libft-checker
+LIBFT_DIR = ..
+SRCS = $(wildcard *.c)
+HDRS = test_libft.h $(wildcard $(LIBFT_DIR)/*.h)
+OBJS = $(SRCS:.c=.o)
+CFLAGS = -Wall -Werror -Wextra
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	gcc $(OBJS) -lft -L$(LIBFT_DIR) -o $(NAME)
+
+%.o: %.c
+	gcc $(CFLAGS) -c $< -o $@ -I$(LIBFT_DIR)
+
+clean:
+	rm -f $(OBJS)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
---
+++ testlibft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testlibft.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcody <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/11 15:45:20 by gcody             #+#    #+#             */
+/*   Updated: 2019/09/11 16:30:41 by gcody            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TESTLIBFT_H
+# define TESTLIBFT_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "libft.h"
+
+int		test_memset(void);
+int		launch_tests(void);
+
+#endif
